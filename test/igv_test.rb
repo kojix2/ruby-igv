5bc304f299bd65cabf3a8df7c7c480b9c44b85ab
kojix2 <2xijok@gmail.com>
2025-04-23 18:45:53 +0900

    Add GitHub Actions CI

--- test/igv_test.rb
+++ test/igv_test.rb
@@ -6,7 +6,12 @@ require 'igv'
 
 class IGVTest < Test::Unit::TestCase
   def setup
-    @igv = IGV.start
+    @igv = case ENV['IGV_TEST_MODE']
+           when 'external'
+             IGV.open
+           else
+             IGV.start
+           end
   end
 
   def test_echo
@@ -15,7 +20,14 @@ class IGVTest < Test::Unit::TestCase
   end
 
   def teardown
-    @igv.kill
-    @igv.close
+    return unless @igv
+
+    case ENV['IGV_TEST_MODE']
+    when 'external'
+      @igv.close
+    else
+      @igv.kill
+      @igv.close
+    end
   end
 end
